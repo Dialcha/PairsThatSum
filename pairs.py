76089d7f5dcc4b2e55f16e14fade64f0a46b19f6
Diego <diego.chavarriap@gmail.com>
2023-08-17 17:48:43 -0500

    PS3-Adding test cases for strings & floats in array, test case for empty array

--- pairs.py
+++ pairs.py
@@ -9,5 +9,3 @@ def pairs_that_sum(numbers_arr, target_value):
                 pairs.append((numbers_arr[i], numbers_arr[j]))
 
     return pairs
-
-#print(pairs_that_sum([1,9,5,0,20,-4,12,16,7], 12))
--- test_pairs.py
+++ test_pairs.py
@@ -18,4 +18,41 @@ def test_pairs_that_sum_12():
     expected_result = [(5, 7), (0, 12), (-4, 16)]
     assert result == expected_result
 
+def test_strings_in_array():
+    numbers_arr = [1,9,5,'no valid',20,-4,12]
+    target_value = 12
 
+    result = pairs_that_sum(numbers_arr, target_value)
+
+    expected_result = 'There is a string in the data, please enter valid array of integers'
+    assert result == expected_result
+
+def test_no_pairs_sum():
+    numbers_arr = [1, 2, 3]
+    target_value = 111
+
+    result = pairs_that_sum(numbers_arr, target_value)
+
+    expected_result = []
+    assert result == expected_result
+
+def test_empty_array():
+    numbers_arr = []
+    target_value = 5
+
+    result = pairs_that_sum(numbers_arr, target_value)
+
+    expected_result = 'Invalid input'
+    assert result == expected_result
+
+def test_pairs_float_numbers():
+    numbers_arr = [1.0, 2.0, 3.0]
+    target_value = 5
+
+    result = pairs_that_sum(numbers_arr, target_value)
+
+    expected_result = 'Float numbers in the array, please enter a valid array of integers'
+    assert result == expected_result
+
+# def test_invalid_input():
+#     # TODO
